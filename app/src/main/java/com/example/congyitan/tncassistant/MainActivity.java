712711454e2a201bba7966c8081c0896d7c9045c
Congyi Tan <congyi@gmail.com>
2015-10-18 21:44:40 +0800

    Added buttons

--- app/src/main/java/com/example/congyitan/tncassistant/MainActivity.java
+++ app/src/main/java/com/example/congyitan/tncassistant/MainActivity.java
@@ -1,13 +1,12 @@
 package com.example.congyitan.tncassistant;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,17 +14,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
@@ -33,8 +22,18 @@ public class MainActivity extends AppCompatActivity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
 
+        Button captureImages = (Button) findViewById(R.id.capture_images);
+        Button generateReport = (Button) findViewById(R.id.generate_report);
 
         return true;
+    }
+
+    public void captureImageActivity(View view){
+        Toast.makeText(MainActivity.this, "Capture Images Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void generateReportActivity(View view){
+        Toast.makeText(MainActivity.this, "Generate Report Button Clicked", Toast.LENGTH_SHORT).show();
     }
 
     @Override
